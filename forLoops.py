55a4fc8d9a92a4be6fbfffc3235c78cfc8974fe1
Glass <rglass8@gatech.edu>
2016-02-19 14:05:16 -0500

    for and while loops

---
+++ forLoops.py
@@ -0,0 +1,18 @@
+officeAssistants = ["Lauren", "Taylor", "Rania"]
+
+for student in officeAssistants:
+    print "Student: ", student
+
+for i in officeAssistants:
+    print "Assistant: ", i
+
+
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in range(5):
+    print "for loop: ", myList[i]
+
+i = myList[0]
+while i > 0:
+    print "While loop: ", i
+    i = myList[i]
